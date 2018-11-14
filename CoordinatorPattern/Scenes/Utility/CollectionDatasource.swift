ec9afe5b2b4061b63b1150b66b93cec9e9270334
bobgodwinx <bobgodwinx@gmail.com>
2018-11-14 18:17:00 +0000

    extended CollectionView to register CollectionRows

--- CoordinatorPattern/Scenes/Utility/CollectionDatasource.swift
+++ CoordinatorPattern/Scenes/Utility/CollectionDatasource.swift
@@ -10,13 +10,28 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+/// Defines the `blueprint`
+/// for a `Row` within the
+/// `UICollectionViewController`
 protocol CollectionRow {
     var configureCell: (UICollectionViewCell) -> Void { get }
     static var cellIdentifier: String {get}
     static var nibName: String { get }
 }
-
+/// a shorthand for `cellIdentifier`
 extension CollectionRow {
     var cellId: String { return Self.cellIdentifier }
 }
 
+/// Registers a `CollectionRow.Type` to the
+/// `UICollectionViewController`
+extension UICollectionView {
+    func register(_ rows: [CollectionRow.Type]) {
+        rows.forEach { register($0) }
+    }
+    
+    func register(_ row: CollectionRow.Type) {
+        let nib = UINib(nibName: row.nibName, bundle: nil)
+        register(nib, forCellWithReuseIdentifier: row.cellIdentifier)
+    }
+}
