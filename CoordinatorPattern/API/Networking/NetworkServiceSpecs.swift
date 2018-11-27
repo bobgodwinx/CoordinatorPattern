5a520b9127db99b4f715b4b9dfb666f7fc2bf915
bobgodwinx <bobgodwinx@gmail.com>
2018-11-27 18:25:00 +0000

    Initial introduction of NetworkServiceSpecs

---
+++ CoordinatorPattern/API/Networking/NetworkServiceSpecs.swift
@@ -0,0 +1,28 @@
+//
+//  NetworkServiceSpecs.swift
+//  CoordinatorPattern
+//
+//  Created by Bob Godwin Obi on 27.11.18.
+//  Copyright © 2018 Bob Godwin Obi. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+import RxTest
+import RxSwift
+@testable import CoordinatorPattern
+
+class NetworkServiceSpecs: QuickSpec {
+    override func spec() {
+        ///Implement Specs
+        
+        beforeEach {
+          /// Setup Properties
+        }
+        
+        afterEach {
+            /// De-init or set to nil 
+        }
+    }
+}
