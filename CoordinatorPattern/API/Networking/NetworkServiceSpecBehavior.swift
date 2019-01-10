3ab1aa5723247a71055202ae3efc0954059ed25a
bobgodwinx <bobgodwinx@gmail.com>
2019-01-10 18:18:00 +0000

    Initial introduction of NetworkServiceSpecBehavior

--- CoordinatorPattern/API/Networking/NetworkServiceSpecBehavior.swift
+++ CoordinatorPattern/API/Networking/NetworkServiceSpecBehavior.swift
@@ -19,6 +19,32 @@ struct NetworkServiceContext {
     let sut: NetworkService
     let scheduler: TestScheduler
     let logger: TestLogger
-    /// Todo: implement a `MockURLSession`
     let session: MockURLSession
+}
+
+class NetworkServiceSpecBehavior: Quick.Behavior<NetworkServiceContext> {
+    override class func spec(_ aContext: @escaping () ->NetworkServiceContext) {
+        var sut: NetworkService!
+        var scheduler: TestScheduler!
+        var logger: TestLogger!
+        var session: MockURLSession!
+        var request: Observable<NetworkResponse>!
+        var response: TestableObserver<NetworkResponse>!
+        
+        beforeEach {
+            let cxt = aContext()
+            scheduler = cxt.scheduler
+            logger = cxt.logger
+            session = cxt.session
+            sut = cxt.sut
+        }
+        
+        afterEach {
+            scheduler = nil
+            logger = nil
+            session = nil
+            sut = nil
+            request = nil
+        }
+    }
 }
