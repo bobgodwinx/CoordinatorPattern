034d67866e88ca4333b36f03747a60dc1dad6df0
bobgodwinx <bobgodwinx@gmail.com>
2019-01-28 13:44:00 +0000

    Introdctions of APIClientSpecs

---
+++ CoordinatorPattern/API/API Client/APIClientSpecs.swift
@@ -0,0 +1,29 @@
+//
+//  APIClientSpecs.swift
+//  CoordinatorPatternTests
+//
+//  Created by Bob Godwin Obi on 28.01.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Quick
+import Nimble
+import RxTest
+import RxSwift
+import RxCocoa
+@testable import CoordinatorPattern
+
+class APIClientSpecs: QuickSpec {
+    override func spec() {
+        
+        beforeEach {
+            /// Setup
+        }
+        
+        afterEach {
+            /// tear down
+        }
+    }
+}
