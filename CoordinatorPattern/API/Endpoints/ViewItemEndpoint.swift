d328daf4eb465bc3ecc096bb2938fe46a744085f
bobgodwinx <bobgodwinx@gmail.com>
2019-02-07 14:03:00 +0000

    Initial introduction of ViewItemEndpoint

---
+++ CoordinatorPattern/API/Endpoints/ViewItemEndpoint.swift
@@ -0,0 +1,33 @@
+//
+//  ViewItemEndpoint.swift
+//  CoordinatorPattern
+//
+//  Created by Bob Godwin Obi on 07.05.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+struct ViewItemEndpoint: Resource {
+    
+    init(parameters: String?) {
+        self.parameters = parameters
+    }
+    
+    typealias Response = ResponseArray<ViewItem>
+    
+    var path: String { return "svc/a" }
+    var method: HTTPMethod { return HTTPMethod.GET }
+    var parameters: String?
+    
+    func parse(networkResponse response: NetworkResponse) throws -> ResponseArray<ViewItem> {
+        let decoder = JSONDecoder()
+        
+        guard let container: ItemContainer = try? decoder.decode(ItemContainer.self, from: response) else {
+            throw APIClient.Error.misformattedResponse
+        }
+        
+        return ResponseArray(content: container.images)
+    }
+}
