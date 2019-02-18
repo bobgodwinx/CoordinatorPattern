ed202c3f44e45cda537a09b224ca0133696b7501
bobgodwinx <bobgodwinx@gmail.com>
2019-02-18 19:14:00 +0000

    Initial implementation for ViewItemProvider Specs

---
+++ CoordinatorPattern/Providers/ViewItemProviderSpecs.swift
@@ -0,0 +1,28 @@
+//
+//  ViewItemProviderSpecs.swift
+//  CoordinatorPatternTests
+//
+//  Created by Bob Godwin Obi on 18.02.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import RxTest
+import RxSwift
+import RxCocoa
+@testable import CoordinatorPattern
+
+class ViewItemProviderSpecs: QuickSpec {
+    override func spec() {
+        
+        beforeEach {
+            /// setup
+        }
+        
+        afterEach {
+            /// tear down
+        }
+    }
+}
