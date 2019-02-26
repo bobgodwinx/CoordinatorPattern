9ceaa33965685232530c715bcc5bcb69d868e025
bobgodwinx <bobgodwinx@gmail.com>
2019-02-26 18:16:00 +0000

    Initial introduction of ViewItemCell

---
+++ CoordinatorPattern/Models/ViewItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  ViewItemCell.swift
+//  CoordinatorPattern
+//
+//  Created by Bob Godwin Obi on 26.02.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+protocol BindableCell {
+    func bind(_ viewModel: ViewItemCellViewModelType)
+}
+
+
+class ViewItemCell: UICollectionViewCell {
+    
+}
