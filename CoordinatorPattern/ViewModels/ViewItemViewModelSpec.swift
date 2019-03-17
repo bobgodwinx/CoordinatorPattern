cd03870d0b5d08fff6f4fda0433357e202ba6308
bobgodwinx <bobgodwinx@gmail.com>
2019-03-17 14:10:00 +0000

    Added implementation of MockViewItemProvider to ViewItemViewModelSpec

--- CoordinatorPattern/ViewModels/ViewItemViewModelSpec.swift
+++ CoordinatorPattern/ViewModels/ViewItemViewModelSpec.swift
@@ -17,3 +17,26 @@ import RxCocoa
 class ViewItemViewModelSpec: QuickSpec {
     
 }
+
+
+class MockViewItemProvider: BaseMock, ViewItemProviderType {
+    
+    lazy var sinkEvents = scheduler.createObserver(String.self)
+    
+    var items: Observable<[ViewItemCellViewModelType]> {
+        let _items = dummyData().map { ViewItemCellViewModel($0) }
+        return scheduler.just(_items).asObservable()
+    }
+    
+    var isFetching: Observable<Bool> {
+        return scheduler
+            .createColdObservable([Recorded.next(0, false),
+                                   Recorded.next(10, true),
+                                   Recorded.next(20, false)])
+            .asObservable()
+    }
+    
+    var sink: AnyObserver<String> {
+        return sinkEvents.asObserver()
+    }
+}
