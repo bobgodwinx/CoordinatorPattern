df449ecfbc2945db777565969cbc9a0b29d6c026
bobgodwinx <bobgodwinx@gmail.com>
2019-03-17 13:20:00 +0000

    Initial introduction of ViewItemViewModelSpec

---
+++ CoordinatorPattern/ViewModels/ViewItemViewModelSpec.swift
@@ -0,0 +1,19 @@
+//
+//  ViewItemViewModelSpec.swift
+//  CoordinatorPattern
+//
+//  Created by Bob Godwin Obi on 17.03.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import RxTest
+import RxSwift
+import RxCocoa
+@testable import CoordinatorPattern
+
+class ViewItemViewModelSpec: QuickSpec {
+    
+}
