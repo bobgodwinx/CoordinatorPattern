fa092aa97a86768b5b162b0bd9d50b0edec274e3
bobgodwinx <bobgodwinx@gmail.com>
2019-03-21 19:22:00 +0000

    Initial introduction of ImageSeceneViewModelSpecs

---
+++ CoordinatorPattern/ViewModels/ImageSeceneViewModelSpecs.swift
@@ -0,0 +1,20 @@
+//
+//  ImageSeceneViewModelSpecs.swift
+//  CoordinatorPatternTests
+//
+//  Created by Bob Godwin Obi on 21.03.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import UIKit
+import Quick
+import Nimble
+import RxTest
+import RxSwift
+import RxCocoa
+@testable import CoordinatorPattern
+
+class ImageSeceneViewModelSpecs: QuickSpec {
+    override func spec() {
+    }
+}
