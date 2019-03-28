385300fa7fffd2ef3ca37f333fbe09ccf3b6a290
bobgodwinx <bobgodwinx@gmail.com>
2019-03-28 18:50:00 +0000

    Introduction of MainCoordinator to AppDelegate

--- CoordinatorPattern/AppDelegate.swift
+++ CoordinatorPattern/AppDelegate.swift
@@ -12,9 +12,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var coordinator: MainCoordinator?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        /// Instantiate the main navigation controller to be used for our app
+        let navController = UINavigationController()
+        navController.navigationBar.isHidden = true
+        //// Instantiate MainCoordinator and inject `navController`
+        coordinator = MainCoordinator(navigationController: navController)
+        /// coordinator start
+        coordinator?.start()
+        /// create UIWindow and makeKeyAndVisible
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
         return true
     }
 
