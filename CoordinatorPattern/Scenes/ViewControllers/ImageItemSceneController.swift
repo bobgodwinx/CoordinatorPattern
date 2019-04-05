732ea8d81d7efa09721f355d1bbb73714afd0226
bobgodwinx <bobgodwinx@gmail.com>
2019-04-05 18:56:00 +0000

    Initial introduction of ImageItemSceneController

---
+++ CoordinatorPattern/Scenes/ViewControllers/ImageItemSceneController.swift
@@ -0,0 +1,31 @@
+//
+//  ImageItemSceneController.swift
+//  CoordinatorPattern
+//
+//  Created by Bob Godwin Obi on 05.04.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class ImageItemSceneController: UIViewController {
+    weak var coordinator: MainCoordinator?
+    private let viewModel: ImageSceneViewModelType
+    private let bag = DisposeBag()
+    
+    
+    init(_ viewModel: ImageSceneViewModelType, _ coordinator: MainCoordinator? ) {
+        self.viewModel = viewModel
+        self.coordinator = coordinator
+        
+        super.init(nibName: nil, bundle: nil)
+        self.view.backgroundColor = .white
+        self.title = "Detail"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+}
